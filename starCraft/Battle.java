1c981927505a95162e9db6c2767086ed96005244
Raeheon <72961952+heon98@users.noreply.github.com>
2022-12-16 13:49:04 +0900

    Update Battle.java

--- starCraft/Battle.java
+++ starCraft/Battle.java
@@ -6,13 +6,13 @@ public class Battle {
 		int enemyPower = Computer.enemyPower;
 		
 		
-		System.out.println("유저 전투력 : "+userPower+", 컴퓨터 전투력 : "+enemyPower+" 입니다.");
+		System.err.println("유저 전투력 : "+userPower+", 컴퓨터 전투력 : "+enemyPower+" 입니다.");
 		if(userPower>enemyPower) {
-			System.out.println("당신의 승리입니다!!");
+			System.err.println("당신의 승리입니다!!");
 		} else if(userPower==enemyPower) {
-			System.out.println("무승부 입니다!!");
+			System.err.println("무승부 입니다!!");
 		} else {
-			System.out.println("당신의 패배입니다!!");
+			System.err.println("당신의 패배입니다!!");
 		}
 	}
 }
